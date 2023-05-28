d0fc69df9f18c77c75a5310edc9b64f69f1b5fb0
Pratik Yadav <ypratik817@gmail.com>
2023-05-28 13:53:33 +0530

    oo

--- handlers/home.py
+++
@@ -1,7 +0,0 @@
-from zenroom import app
-from flask import render_template
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('index.html', title='Guide to Mental WellBeing')
